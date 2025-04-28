f5d7f581d90433382c12d9453809b3f32d1c3367
Deck-Wang416 <sc212yw@leeds.ac.uk>
2025-04-28 23:24:18 +0800

    feat: finished login api.

--- prof_rating_service/ratings/urls.py
+++ prof_rating_service/ratings/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ModuleInstanceListView, ProfessorListView, ProfessorModuleAverageRatingView, RateProfessorView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('modules/', ModuleInstanceListView.as_view(), name='module-list'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('professors/<str:professor_id>/module/<str:module_code>/average/', ProfessorModuleAverageRatingView.as_view(), name='professor-module-average'),
     path('rate/', RateProfessorView.as_view(), name='rate-professor'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
