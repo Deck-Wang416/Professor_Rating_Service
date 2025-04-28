1901fdde4e7d9420aeb50101cca371b3e2c95fa7
Deck-Wang416 <sc212yw@leeds.ac.uk>
2025-04-28 17:12:52 +0800

    feat: finished admin backstage management.

--- prof_rating_service/ratings/admin.py
+++ prof_rating_service/ratings/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Professor, Module, ModuleInstance, Rating
 
-# Register your models here.
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+    search_fields = ('code', 'name')
+
+@admin.register(ModuleInstance)
+class ModuleInstanceAdmin(admin.ModelAdmin):
+    list_display = ('module', 'year', 'semester')
+    list_filter = ('year', 'semester')
+    filter_horizontal = ('professors',)
+    search_fields = ('module__code', 'module__name')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'professor', 'module_instance', 'rating')
+    list_filter = ('rating',)
+    search_fields = ('professor__name', 'module_instance__module__name', 'user__username')
