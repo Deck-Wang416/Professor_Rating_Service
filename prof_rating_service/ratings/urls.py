d6068c4ffe96c2fabe783440ba21183c641c692c
Deck-Wang416 <sc212yw@leeds.ac.uk>
2025-04-28 22:43:29 +0800

    feat: finished get professors rating in one module.

--- prof_rating_service/ratings/urls.py
+++ prof_rating_service/ratings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ModuleInstanceListView, ProfessorListView
+from .views import ModuleInstanceListView, ProfessorListView, ProfessorModuleAverageRatingView
 
 urlpatterns = [
     path('modules/', ModuleInstanceListView.as_view(), name='module-list'),
     path('professors/', ProfessorListView.as_view(), name='professor-list'),
+    path('professors/<str:professor_id>/module/<str:module_code>/average/', ProfessorModuleAverageRatingView.as_view(), name='professor-module-average'),
 ]
--- prof_rating_service/ratings/views.py
+++ prof_rating_service/ratings/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import ModuleInstance, Professor
+from rest_framework import status
+from .models import ModuleInstance, Professor, Module, Rating
 from .serializers import ModuleInstanceSerializer, ProfessorWithRatingSerializer
 
 class ModuleInstanceListView(APIView):
@@ -14,3 +15,30 @@ class ProfessorListView(APIView):
         professors = Professor.objects.all()
         serializer = ProfessorWithRatingSerializer(professors, many=True)
         return Response(serializer.data)
+
+class ProfessorModuleAverageRatingView(APIView):
+    def get(self, request, professor_id, module_code):
+        try:
+            module = Module.objects.get(code=module_code)
+        except Module.DoesNotExist:
+            return Response({"error": "Module not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        ratings = Rating.objects.filter(
+            professor__id=professor_id,
+            module_instance__module=module
+        )
+
+        if ratings.exists():
+            avg = sum(r.rating for r in ratings) / ratings.count()
+            avg = round(avg)
+            return Response({
+                "professor_id": professor_id,
+                "module_code": module_code,
+                "average_rating": avg
+            })
+        else:
+            return Response({
+                "professor_id": professor_id,
+                "module_code": module_code,
+                "average_rating": None
+            })
